80d5d481e7564a813010e7f9e45509666cb5dde8
Konrad Chomiak <konrad.siema2@gmail.com>
2021-11-24 15:18:41 +0100

    initial

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+console.log('siema')
+const name = 'konrad'
+const age = ('23');
+console.log(name);
+console.log(age);
+
+console.log(`mam na imie ${name} i mam ${age}lata`)
+
+alert('wypierdalaj')
