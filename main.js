a3155f42178088c894a5e143d715d6e60c990e50
Konrad Chomiak <konrad.siema2@gmail.com>
2021-11-26 12:56:43 +0100

    changes

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
 
 
 console.log('siema')
-const name = 'konrad'
-const age = ('23');
-console.log(name);
+const username = 'konrad'
+const age = ('21');
+console.log(username);
 console.log(age);
 
-console.log(`mam na imie ${name} i mam ${age}lata`)
+console.log(`mam na imie ${username} i mam ${age}lat`)
 
+if (age > 23) {
+    console.log('masz wiecej niz 23 lata');
+} else {
+    console.log('masz mniej niz 23 lata')
+}
+
+
+if (username === 'konrad') {
+    console.log('nazywasz sie konrad');
+} else {
+    console.log(`nie nazywasz sie Konrad `)
+}
+
+
+if (username !== 'konrad') {
+ 
+console.log( 'nie jestes konrad narka')}
+
+else  {
+    console.log ('wilkomen to liberty city')}
+
+
+
+const rukonrad = (age > 23) ? 'masz wiecej lat niz konrader' : 'masz mniej lat niz kontal';
+console.log(rukonrad);
